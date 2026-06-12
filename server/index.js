9b269f7caff6c9c9e154548b1cd47959a6d2938f
Vindhya S <vindhya2003s@gmail.com>
2026-06-12 18:08:46 +0530

    Databse changes

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB error:', err));
 
+
 // Nodemailer transporter
 const transporter = nodemailer.createTransport({
   service: 'gmail',
